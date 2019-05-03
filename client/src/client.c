e7125cc2e8a45edbcd964822ba5fffc06093a311
BARLOY Nathan <nathan.barloy@telecomnancy.eu>
2019-05-03 17:02:18 +0200

    refonte totale du client. Code plus clair. Interactions entre client et serveur

--- client/src/client.c
+++ client/src/client.c
@@ -10,133 +10,13 @@
 #include <unistd.h>
 #include <termios.h>
 
-#define _XOPEN_SOURCE 600
-#define MAXLINE 1024
-#define TAILLEMAX 20
+#define MAXLINE 140
 
-/*
-Serveur TCP :
-
-Socket => Bind => Listen => Accept => Exchange (reader/writer) => Close
-*/
-
-
-
-int compter_thematiques(char *texte) {
-  int nbThematique = 0;
-  int dansUnTheme=0;
-  int tailleTheme=0;
-  int i = -1;
-  char c;
-
-  do {
-
-    i++;
-      c = texte[i];
-
-    switch(c) {
-
-      case '#' :
-        nbThematique++;
-          dansUnTheme = 1;
-          tailleTheme = 0;
-          break;
-
-        case ' ' :
-
-        case '\0' :
-          if (dansUnTheme) {
-            dansUnTheme = 0;
-            //printf("debut : %d et taille : %d\n", debutTheme,tailleTheme);
-            if (tailleTheme==0) {
-              nbThematique--;
-            }
-          }
-          break;
-
-        default :
-          if (dansUnTheme) {
-            tailleTheme++;
-          }
-          break;
-
-      } 
-
-      
-    } while (c!='\0');
-
-    return nbThematique;
-}
-
-char **recherche_thematiques(char *texte) {
-  /*cette fonction sert à extraire les thematiques d'un tweet*/
-
-  int nbThematique = compter_thematiques(texte);
-
-  char **res = malloc(sizeof(char*)*nbThematique);
-  int dansUnTheme=0;
-  int tailleTheme=0;
-  int debutTheme=0;
-  int i = -1;
-  char c;
-  char *newTheme;
-  int numTheme = 0;
-
-  do {
-
-    i++;
-      c = texte[i];
-
-    switch(c) {
-
-      case '#' :
-        if (dansUnTheme) {
-          dansUnTheme = 0;
-            //printf("debut : %d et taille : %d\n", debutTheme,tailleTheme);
-            if (tailleTheme==0) {
-              numTheme--;
-            } else {
-              newTheme = malloc(sizeof(char)*(tailleTheme+1));
-              strncpy(newTheme,texte+debutTheme, tailleTheme);
-              //printf("%s\n", newTheme);
-              res[numTheme-1] = newTheme;
-            }
-        }
-        numTheme++;
-          dansUnTheme = 1;
-          debutTheme = i+1;
-          tailleTheme = 0;
-          break;
-
-        case ' ' :
-
-        case '\0' :
-          if (dansUnTheme) {
-            dansUnTheme = 0;
-            //printf("debut : %d et taille : %d\n", debutTheme,tailleTheme);
-            if (tailleTheme==0) {
-              numTheme--;
-            } else {
-              newTheme = malloc(sizeof(char)*(tailleTheme+1));
-              strncpy(newTheme,texte+debutTheme, tailleTheme);
-              //printf("%s\n", newTheme);
-              res[numTheme-1] = newTheme;
-            }
-          }
-          break;
-
-        default :
-          if (dansUnTheme) {
-            tailleTheme++;
-          }
-          break;
-
-      } 
-
-      
-    } while (c!='\0');
-    return res;
-}
+int serverSocket,servlen,n,retread;
+struct sockaddr_in serv_addr;
+char bufSend[MAXLINE];
+char bufRecv[MAXLINE];
+struct hostent *hp;
 
 
 void viderBuffer()
@@ -148,308 +28,240 @@ void viderBuffer()
     }
 }
 
-
-void usage(){
-	printf("usage : clieecho adresse_ip_server numero_port_serveur \n");
+int affInfo(char *buff) {
+    printf("type : %d\tcontenu : %s\n",(int)buff[0], &buff[1]);
 }
 
-int writen (fd, ptr, nbytes)
-     int  fd;
-     char *ptr;
-     int nbytes;
-{
-  int nleft, nwritten; 
-  char *tmpptr;
+void afficherMascotte() {
 
-  nleft = nbytes;
-  tmpptr=ptr;
-  while (nleft >0) {
-    nwritten = write(fd,ptr, nleft);
-    if (nwritten <=0) {
-      if(errno == EINTR)
-  nwritten=0;
-      else{
-  perror("probleme  dans write\n");
-  return(-1);
-      }
-    }
-    nleft -= nwritten;
-    ptr += nwritten;
-  }
-  return (nbytes);
+	char *turkey = "\n -----------------------------------------\n\
+/ Bienvenue sur lca contrefaçon Twitter ! \\\n\
+| Une application au top de la            |\n\
+\\ technologie !                           /\n\
+ -----------------------------------------\n\
+  \\                                  ,+*^^*+___+++_\n\
+   \\                           ,*^^^^              )\n\
+    \\                       _+*                     ^**+_\n\
+     \\                   +^       _ _++*+_+++_,         )\n\
+              _+^^*+_    (     ,+*^ ^          \\+_        )\n\
+             {       )  (    ,(    ,_+--+--,      ^)      ^\\\n\
+            { (@)    } f   ,(  ,+-^ __*_*_  ^^\\_   ^\\       )\n\
+           {:;-/    (_+*-+^^^^^+*+*<_ _++_)_    )    )      /\n\
+          ( /  (    (        ,___    ^*+_+* )   <    <      \\\n\
+           U _/     )    *--<  ) ^\\-----++__)   )    )       )\n\
+            (      )  _(^)^^))  )  )\\^^^^^))^*+/    /       /\n\
+          (      /  (_))_^)) )  )  ))^^^^^))^^^)__/     +^^\n\
+         (     ,/    (^))^))  )  ) ))^^^^^^^))^^)       _)\n\
+          *+__+*       (_))^)  ) ) ))^^^^^^))^^^^^)____*^\n\
+          \\             \\_)^)_)) ))^^^^^^^^^^))^^^^)\n\
+           (_             ^\\__^^^^^^^^^^^^))^^^^^^^)\n\
+             ^\\___            ^\\__^^^^^^))^^^^^^^^)\\\n\
+                  ^^^^^\\uuu/^^\\uuu/^^^^\\^\\^\\^\\^\\^\\^\\^\\\n\
+                     ___) >____) >___   ^\\_\\_\\_\\_\\_\\_\\)\n\
+                    ^^^//\\_^^//\\_^       ^(\\_\\_\\_\\)\n\
+                      ^^^ ^^ ^^^ ^\n";
+
+
+
+
+    printf("%s\n",turkey);
 }
 
 
-/*
- * Lire  "n" octets à partir d'un descripteur de socket
- */
-int readn (fd, ptr, maxlen)
-     int  fd;
-     char *ptr;
-     int maxlen;
-{
-  char *tmpptr;
-  int nleft, nreadn;
 
-  nleft = maxlen;
-  tmpptr=ptr;
-  
-  while (nleft >0) {
-    nreadn = read (fd,ptr, nleft);
-    if (nreadn < 0) {
-      if(errno == EINTR)
-  nreadn=0;
-      else{
-  perror("readn : probleme  dans read \n");
-  return(-1);
-      }
-    }
-    else if(nreadn == 0){
-      /* EOF */ 
-      break ;
-    }
-    nleft -= nreadn;
-    ptr += nreadn;
-  }
-  return (maxlen - nleft);
+int envoyer(int type, char* message) {
+	int send_size = (strlen(message) + 2) * sizeof(char);
+	char* send_buff = malloc(send_size);
+	send_buff[0] = type;
+	strcpy(&send_buff[1], message);
+	if (send(serverSocket, send_buff, send_size, 0) >= 2) {
+		send_size = 0;
+	} else {
+		send_size = 1;
+	}
+	free(send_buff);
+	return send_size;
 }
 
-/*
- * Lire  une ligne terminee par \n à partir d'un descripteur de socket
- */
-int readline (fd, ptr, maxlen)
-     int  fd;
-     char *ptr;
-     int maxlen;
-{
-  
-  int n, rc, retvalue, encore=1;  char c, *tmpptr; 
-
-  tmpptr=ptr;
-  for (n=1; (n < maxlen) && (encore) ; n++) {
-    if ( (rc = read (fd, &c, 1)) ==1) {
-      *tmpptr++ =c; 
-      if (c == '\n')  /* fin de ligne atteinte */
-  {encore =0; retvalue = n;}
-    }
-    else if (rc ==0) {  /* plus rien à lire */
-      encore = 0;
-      if (n==1) retvalue = 0;  /* rien a été lu */
-      else retvalue = n;
-    }
-    else { /*rc <0 */
-      if (errno != EINTR) {
-  encore = 0;
-  retvalue = -1;
-      }
-    }
-  }
-  *tmpptr = '\0';  /* pour terminer la ligne */
-  return (retvalue);
+int quitter() {
+	envoyer(2,"");
+	close(serverSocket);
+	exit(0);
 }
 
 int menuConnecte(char *addIp, int port) {
-  char answer;
-  int cont = 0;
-  printf("\nQue voulez vous faire ?\n");
-  while (!cont) {
-    printf("t -> twitter\nu -> demander la liste des utilisateurs suivis\nv -> demander la liste des utilisateurs qui vous suivent\nh -> demander la liste des thématiques suivies\nd -> déconnexion\nq -> quitter l'application\nVotre choix : ");
-    answer = getchar();
-    if (answer && (answer=='t' || answer=='u' || answer=='v' || answer=='h' || answer=='d' || answer=='q')) {
-      cont = 1;
-    } else {
-      printf("\nVotre entrée n'est pas correcte. Réessayez.\n");
-    }
-    viderBuffer();
-  }
-  switch (answer) {
-    case 't' :
-      break;
-    case 'u' :
-      break;
-    case 'v' :
-      break;
-    case 'h' :
-      break;
-    case 'd' :
-      break;
-    case 'q' :
-      printf("quitter\n");
-      exit(0);
-      break;
-  }
+  	char answer;
+  	int cont = 0;
+  	printf("\nQue voulez vous faire ?\n");
+  	while (!cont) {
+    	printf("t -> twitter\nu -> demander la liste des utilisateurs suivis\nv -> demander la liste des utilisateurs qui vous suivent\nh -> demander la liste des thématiques suivies\nd -> déconnexion\nq -> quitter l'application\nVotre choix : ");
+    	answer = getchar();
+    	if (answer && (answer=='t' || answer=='u' || answer=='v' || answer=='h' || answer=='d' || answer=='q')) {
+     		cont = 1;
+    	} else {
+      		printf("\nVotre entrée n'est pas correcte. Réessayez.\n");
+    	}
+    		viderBuffer();
+  	}
+  	switch (answer) {
+    	case 't' :
+      		break;
+    	case 'u' :
+      		break;
+    	case 'v' :
+      		break;
+    	case 'h' :
+      		break;
+    	case 'd' :
+      		break;
+    	case 'q' :
+      		quitter();
+      		break;
+  	}
 
-  return 0;
+  	return 0;
 }
 
+void creer_compte() {
+	char nomUtil[141];
+	char mdp[141];
+	char confmdp[141];
+	char *toSend;
+	int Cbon=0;
+	int recv_size;
+
+	printf("\n\n");
+	while(!Cbon) {
+		printf("Votre nom d'utilisateur :");
+    	scanf("%140s",nomUtil);
+    	printf("Votre mot de passe :");
+    	scanf("%140s",mdp);
+    	printf("Confirmation du mot de passe :");
+    	scanf("%140s",confmdp);
+
+    	if (strcmp(mdp,confmdp)==0 && nomUtil!=NULL && nomUtil!="") { //les données sont conformes
+      		Cbon = 1;
+      		toSend = malloc(sizeof(char)*(strlen(nomUtil)+strlen(mdp)+2));
+
+      		strcpy(toSend,nomUtil);
+      		strcat(toSend,"@");
+      		strcat(toSend,mdp);
+
+      		envoyer(0,toSend);
+
+      		if ((recv_size = recv(serverSocket, bufRecv, MAXLINE, 0)) >= 2) {
+      			bufRecv[recv_size] = '\0';
+				int type = bufRecv[0];
+				char* message = &bufRecv[1];
+				affInfo(bufRecv);
+      		}
+
+      		menuPrincipal();
+    	} else {
+      		printf("\nVos données sont non conformes\n");
+    	}
+  	}
+  	viderBuffer();
+
+}
 
 
 int connexion(char *addIp, int port) {
-  /*se connecte au serveur situé à l'adresse désignée par les paramètres*/
-  int continuer = 1; //booleen qui indique si la boucle doit se finir ou non
-  int serverSocket,servlen,n;
-  fd_set readfds;
-  struct sockaddr_in serv_addr;
-  char bufServ[MAXLINE];
-  char bufUser[MAXLINE];
-  struct hostent *hp;
 
-//on remplis la structure serv_addr avec l'adresse du serveur
-  bzero((char *) &serv_addr, sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET;
-  int portno = port;
-      serv_addr.sin_port = htons(portno);
-  hp=(struct hostent*)gethostbyname(addIp);
-  if(hp==NULL){
-    fprintf(stderr,"client : %s ,p, trouve dans in /etc/hosts ou dans le DNS\n",addIp);
-    exit(0);
-  }
-  serv_addr.sin_addr = *((struct in_addr *)(hp->h_addr));
-  //printf("IP address :%s\n",inet_ntoa(serv_addr.sin_addr));
-  
-  /*
-  ouvrire socket (socket stream)
-  */
+	//on remplis la structure serv_addre avec l'adresse du serveur
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	int portno = port;
+    serv_addr.sin_port = htons(portno);
+	hp=(struct hostent*)gethostbyname(addIp);
+	if(hp==NULL){
+		fprintf(stderr,"%s : %d ,p, trouve dans in /etc/hosts ou dans le DNS\n",addIp,port);
+		exit(0);
+	}
+	serv_addr.sin_addr = *((struct in_addr *)(hp->h_addr));
 
-  if((serverSocket = socket(AF_INET,SOCK_STREAM,0))<0){
-    perror("erreur socket");
-    exit(1);
-  }
-  //printf("socket ok\n");
-  
-  if(connect(serverSocket,(struct sockaddr *) &serv_addr,sizeof(serv_addr))<0){
-    perror("erreur connect");
-    exit(1);
-  }
-  //printf("connect ok\n");
-
-  while(continuer){
-
-    memset(bufServ,'\0',MAXLINE);
-    memset(bufUser,'\0',MAXLINE);
-
-    FD_ZERO(&readfds);    // il faut remettre tous les elements des readfds a chaque recommencement de la boucle, vu que select modifie les ensembles
-    FD_SET(0,&readfds);       // on rajoute l'entree standard
-    FD_SET(serverSocket,&readfds);   // on rajoute la socket de communication avec le serveur
-    
-    if(select(serverSocket+1,&readfds,NULL,NULL,NULL) == -1){
-      perror("Erreur lors de l'appel a select -> ");
-      exit(1);
-    }
-
-    
-
-    //if (FD_ISSET(0,&readfds)) { //on ecrit dans le stdin
-
-      menuConnecte(addIp, port);
-
-      /*
-      printf("vous : ");
-      if(fgets(bufUser,MAXLINE,stdin)==NULL){
-        perror("erreur fgets\n");
-        exit(1);
-      }
-      //Envoyer le message au server
-        if((n=writen(serverSocket,bufUser,strlen(bufUser))) != strlen(bufUser)){   
-        printf("erreur writen");
-        exit(0);
-      }
-      */
-    //}
-
-    if (FD_ISSET(serverSocket,&readfds)) { //on a recu un message du serveur
-      if (readline(serverSocket,bufServ,MAXLINE)<0) {
-        perror("Erreur de reception");
-        exit(1);
-      }
-      printf("tweet recu  : %s", bufServ);
-      printf("\n");
-    }
-
-  }
-  close(serverSocket);
-  return 0;
+	//on crée la socket
+	if((serverSocket = socket(AF_INET,SOCK_STREAM,0))<0){
+		perror("erreur socket");
+		exit(1);
+	}
+	
+	//on connecte la socket
+	if(connect(serverSocket,(struct sockaddr *) &serv_addr,sizeof(serv_addr))<0){
+		perror("erreur connect");
+		exit(1);
+	}
 }
 
 
-int menuPrincipal(char *addIp, int port) {
-  /* lance le menu principal de l'appli
+int menuPrincipal() {
+	  /* lance le menu principal de l'appli
   */
-  char answer;
-  char name[TAILLEMAX];
-  char password[TAILLEMAX];
-  char *animal;
-  char *texte;
-  char commande[TAILLEMAX];
-  int cont = 0;
+	char answer;
+	char name[MAXLINE];
+  	char password[MAXLINE];
+  	int cont = 0;
 
-  animal = "turkey";
-  texte = "Bienvenue sur le Twitter du pauvre ! Une application au top de la technologie !\n";
-  sprintf(commande, "cowsay -f %s %s",animal, texte);
-  system(commande);
-  printf("\nQue voulez vous faire ?\n");
-  while (!cont) {
-    printf("c -> connexion\nn -> créer un nouveau compte\nq -> quitter l'application\nVotre choix : ");
-    answer = getchar();
-    if (answer && (answer=='c' || answer=='n' || answer=='q')) {
-      cont = 1;
-    } else {
-      printf("\nVotre entrée n'est pas correcte. Réessayez.\n");
-    }
-    viderBuffer();
-  }
-  switch (answer) {
-    case 'c' :
-      
-      printf("\nNom d'utilisateur : ");
-      fflush(stdout);
-      scanf("%s", name);
-      fflush(stdout);
-      printf("Mot de passe : ");
-      fflush(stdout);
-      system("stty -echo");
-      scanf("%s", password);
-      system("stty echo");
-      printf("\n%s\n%s\n",name,password);
-      connexion(addIp,port);
-      break;
-    case 'n' :
-      printf("nouveau\n");
-      break;
-    case 'q' :
-      printf("quitter\n");
-      exit(0);
-      break;
-  }
 
-  return 0;
+
+  	printf("\nQue voulez vous faire ?\n");
+  	while (!cont) {
+    	printf("c -> connexion\nn -> créer un nouveau compte\nq -> quitter l'application\nVotre choix : ");
+    	answer = getchar();
+    	if (answer && (answer=='c' || answer=='n' || answer=='q')) {
+      		cont = 1;
+    	} else {
+      		printf("\nVotre entrée n'est pas correcte. Réessayez.\n");
+    	}
+    	viderBuffer();
+	}
+	switch (answer) {
+    	case 'c' :
+      		printf("\nNom d'utilisateur : ");
+      		scanf("%s", name);
+      		printf("Mot de passe : ");
+	      	system("stty -echo");
+	      	scanf("%s", password);
+	      	system("stty echo");
+	      	printf("\n%s\n%s\n",name,password);
+	      	break;
+    	case 'n' :
+	      	creer_compte();
+	      	break;
+	    case 'q' :
+	      	quitter();
+	      	break;
+  	}
+
+  	return 0;
   
 }
-
 
 
 int main(int argc, char **argv){
+	char *addIp = "127.0.0.1";
+	int port = 2222;
 
-  char *addIp = "127.0.0.1";
-  int port = 2222;
+	switch (argc) {
+    	case 1 :
+      		break;
+    	case 2 :
+      		addIp = argv[1];
+      		break;
+    	case 3 :
+      		addIp = argv[1];
+      		port = atoi(argv[2]);
+      		break;
+    	default :
+      		printf("Trop d'arguments\n");
+      		exit(1);
+	}
 
-  switch (argc) {
-    case 1 :
-      break;
-    case 2 :
-      addIp = argv[1];
-      break;
-    case 3 :
-      addIp = argv[1];
-      port = atoi(argv[2]);
-      break;
-    default :
-      printf("Trop d'arguments\n");
-      exit(1);
-  }
+	connexion(addIp, port);
 
-  menuPrincipal(addIp, port);
+	afficherMascotte();
+	menuPrincipal();
 
-  return 0;
+	quitter();
+	return 0;
 }
-
